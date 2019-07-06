789be0d3e2c2c6b19409d4859df8d29daec2ee8c
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-06 14:22:47 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: "siema", letterMatchCount: 5 }]}
+      />
     </div>
   );
 }
