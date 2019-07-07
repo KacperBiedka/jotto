68a5752aeb5231d80d7c9bde3269a2c1cea19364
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-07 17:21:42 +0200

    Added Redux test files

---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,13 @@
+import { actionTypes } from "../actions";
+import successReducer from "./successReducer";
+
+test("return default initial state when no action is passed", () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+test("return state of true upon receiving an action of type `CORRECT_GUESS`", () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS
+  });
+  expect(newState).toBe(true);
+});
