63c138f013d34ef958d4e0aacf07dca91e014d92
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-07 17:40:58 +0200

    Added the Input component

---
+++ src/Input.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Input extends Component {
+  state = {};
+  render() {
+    return <div />;
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);
