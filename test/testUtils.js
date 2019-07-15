b55e09b25d8710777453cf0d51a188988ed1f2f9
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-15 14:10:14 +0200

    Installed redux-thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,14 @@
 import CheckPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../src/reducers";
+import { middlewares } from "../src/configureStore";
 
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAttr = (wrapper, val) => {
