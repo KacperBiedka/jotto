d2f7bc9fe40d5433d3c5ab3917ddbba698a900c0
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-15 14:42:26 +0200

    Started working on the GuessedWords reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,8 @@
 export const actionTypes = {
-  CORRECT_GUESS: "CORRECT_GUESS"
+  CORRECT_GUESS: "CORRECT_GUESS",
+  GUESS_WORD: "GUESS_WORD"
 };
 
-export function correctGuess() {
-  return {
-    type: actionTypes.CORRECT_GUESS
-  };
-}
+export const guessWord = guessedWord => {
+  return function(dispatch, getState) {};
+};
