2d4c635ae1bcf76de6bc54596fdfda550282ab6a
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-18 15:42:34 +0200

    Added test for action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,6 +27,11 @@ export const guessWord = guessedWord => {
 
 export const getSecretWord = () => {
   return dispatch => {
-    axios.get("http://localhost:3030");
+    return axios.get("http://localhost:3030").then(response => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.data
+      });
+    });
   };
 };
---
+++ src/actions/index.test.js
@@ -0,0 +1,30 @@
+import moxios from "moxios";
+
+import { storeFactory } from "../../test/testUtils";
+import { getSecretWord } from "./";
+
+describe("secretWord action creator", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  it("adds response word to state", () => {
+    const secretWord = "party";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      });
+    });
+
+    return store.dispatch(getSecretWord()).then(() => {
+      const newState = store.getState();
+      expect(newState.secretWord).toBe(secretWord);
+    });
+  });
+});
