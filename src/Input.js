dd8a5452e8df3b5299cf8a42d9059aee4f3ca2e7
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-21 15:22:58 +0200

    Added the random-word-server

--- src/Input.js
+++ src/Input.js
@@ -11,6 +11,7 @@ export class UnconnectedInput extends Component {
     if (guessedWord && guessedWord.length > 0) {
       this.props.guessWord(guessedWord);
     }
+    this.inputBox.current.value = "";
   };
 
   render() {
--- src/Input.test.js
+++ src/Input.test.js
@@ -92,4 +92,7 @@ describe("`guessWord` action creator call", () => {
     const guessWordArg = guessWordMock.mock.calls[0][0];
     expect(guessWordArg).toBe(guessedWord);
   });
+  test("input box clears on submit", () => {
+    expect(wrapper.instance().inputBox.current.value).toBe("");
+  });
 });
