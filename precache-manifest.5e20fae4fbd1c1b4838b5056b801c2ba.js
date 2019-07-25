1f46a706f577b14af2f9acc9d30d07f0dc4e4b34
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-07-25 13:03:08 +0200

    Updates

---
+++ precache-manifest.5e20fae4fbd1c1b4838b5056b801c2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7e2d688a1174c57082b9e8a70a50eb",
+    "url": "/jotto/index.html"
+  },
+  {
+    "revision": "7af9968ef617745d5fe8",
+    "url": "/jotto/static/css/main.6e5fd485.chunk.css"
+  },
+  {
+    "revision": "bbf4a8de4b1099eed427",
+    "url": "/jotto/static/js/2.ead2c0fb.chunk.js"
+  },
+  {
+    "revision": "7af9968ef617745d5fe8",
+    "url": "/jotto/static/js/main.c3eeb82d.chunk.js"
+  },
+  {
+    "revision": "fd672d2da2f3ee7b4f3c",
+    "url": "/jotto/static/js/runtime~main.1e445bdb.js"
+  }
+]);
